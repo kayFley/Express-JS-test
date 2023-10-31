cc1d5857bd9bbb02b2e326b67977495b2591854b
-kF <134998691+kayFley@users.noreply.github.com>
2023-11-01 00:18:00 +0700

    Update PostController.js

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -4,10 +4,7 @@ export const getLastTags = async (req, res) => {
     try {
         const posts = await PostModel.find().limit(5).exec();
 
-        const tags = posts
-            .map((obj) => obj.tags)
-            .flat()
-            .slice(0, 5);
+        const tags = posts.map((obj) => obj.tags).flat().slice(0, 5);
 
         res.json(tags);
     } catch (err) {
